fc1dac69441486059e0e5ecaf2b57913c7714889
Rui OAIX <rui.oaix@gmail.com>
2017-07-09 14:50:02 +0200

    test travis ci

--- tests/test_info.py
+++ tests/test_info.py
@@ -1,3 +1,15 @@
-from xqportfolio import info
+import pytest
+from snowman import Info
+from snowman import exceptions
 
-info.Info(1086203).all()
+
+def test_info_not_exist_attribute():
+    symbol = 'ZH123456'
+    info = Info(symbol).get(origin = True)
+    with pytest.raises(AttributeError):
+        info.nonexist
+
+def test_info_not_exist_portfolio():
+    symbol = 'ZH1'
+    with pytest.raises(exceptions.WrongStatusCode):
+        Info(symbol).get()
